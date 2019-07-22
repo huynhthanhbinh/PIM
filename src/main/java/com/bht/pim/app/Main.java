f9ec03371d992dd6d994a1dce906f3302d293935
BHT <46950058+huynhthanhbinh@users.noreply.github.com>
2019-07-22 14:24:54 +0700

    layout intro

--- src/main/java/com/bht/pim/app/Main.java
+++ src/main/java/com/bht/pim/app/Main.java
@@ -4,30 +4,62 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Objects;
 
+/*
+* First thing to do for every main class of FX application
+* is to extends Application class
+* get functionality from the class Application of package javafx
+*/
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
+    /*
+    * launch method setting everything up, and then
+    * it calls a method names start to run the application
+    * We need to @Override it from parent class Application
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-        ClassLoader classLoader  = getClass().getClassLoader();
+
+    /*
+    * FX terminology ???
+    * The main window --> primaryStage
+    * The entire window --> stage
+    * Content inside window --> scene
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        ClassLoader classLoader = getClass().getClassLoader();
 
         Parent root = FXMLLoader
                 .load(Objects.requireNonNull(
                         classLoader.getResource("sample.fxml")));
 
-        Scene scene = new Scene(root, 500, 250);
+        Scene scene = new Scene(root, 400, 200);
 
-        primaryStage.setTitle("Hello World");
+        /*
+        * A layout means how we arrange all the things in the window
+        * How we configure it ?
+        * Since FX 2.0 we have 2 ways to configure :
+        *       one is using java code
+        *       the other is using XML using a file format fxml
+         */
+
+        primaryStage.setTitle("Project Information Management");
+        primaryStage.getIcons().add(
+                new Image(Objects.requireNonNull(
+                        classLoader.getResourceAsStream("pictures/icon.png"))));
+
+        primaryStage.setResizable(true);
+        primaryStage.setMinWidth(250);
+        primaryStage.setMinHeight(150);
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
