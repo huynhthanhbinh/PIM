4dc90825c11d27ae05854214b104b9b509bf0fc7
BHT <46950058+huynhthanhbinh@users.noreply.github.com>
2019-07-22 10:16:56 +0700

    Hello World

--- src/sample/SampleController.java
+++ src/sample/SampleController.java
@@ -1,4 +1,13 @@
 package sample;
 
+// lib for using label in FX
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class SampleController {
+    public Label helloWorld;
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World");
+    }
 }
