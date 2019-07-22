71b862891eaca8382eaa083a4da8d6193abde588
BHT <46950058+huynhthanhbinh@users.noreply.github.com>
2019-07-22 15:22:32 +0700

    Using lambda expression

--- src/main/java/com/bht/pim/app/Main.java
+++ src/main/java/com/bht/pim/app/Main.java
@@ -1,8 +1,11 @@
+/*
+ * @Author  : BHT
+ * @Company : ELCA VN
+ */
+
 package com.bht.pim.app;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -13,19 +16,18 @@ import org.apache.log4j.Logger;
 import java.util.Objects;
 
 /*
-* First thing to do for every main class of FX application
-* is to extends Application class
-* get functionality from the class Application of package javafx
-*/
-public class Main extends Application implements EventHandler<ActionEvent> {
+ * First thing to do for every main class of FX application
+ * is to extends Application class
+ * get functionality from the class Application of package javafx
+ */
+public class Main extends Application {
 
     private Logger logger = Logger.getLogger(Main.class);
-    private Button myButton;
 
     /*
-    * launch method setting everything up, and then
-    * it calls a method names start to run the application
-    * We need to @Override it from parent class Application
+     * launch method setting everything up, and then
+     * it calls a method names start to run the application
+     * We need to @Override it from parent class Application
      */
     public static void main(String[] args) {
         launch(args);
@@ -33,13 +35,13 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 
 
     /*
-    * FX terminology ???
-    * The main window --> primaryStage
-    * The entire window --> stage
-    * Content inside window --> scene
+     * FX terminology ???
+     * The main window --> primaryStage
+     * The entire window --> stage
+     * Content inside window --> scene
      */
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
 
         ClassLoader classLoader = getClass().getClassLoader();
 
@@ -47,9 +49,9 @@ public class Main extends Application implements EventHandler<ActionEvent> {
                 .load(Objects.requireNonNull(
                         classLoader.getResource("sample.fxml")));*/
 
-        myButton = new Button();
+        Button myButton = new Button();
         myButton.setText("Say 'Hello World'");
-        myButton.setOnAction(this);
+        myButton.setOnAction(event -> logger.info("Button say Hello World is clicked !"));
 
         StackPane layout = new StackPane();
         layout.getChildren().add(myButton);
@@ -59,11 +61,11 @@ public class Main extends Application implements EventHandler<ActionEvent> {
                 classLoader.getResource("sample.css")).toExternalForm());
 
         /*
-        * A layout means how we arrange all the things in the window
-        * How we configure it ?
-        * Since FX 2.0 we have 2 ways to configure :
-        *       one is using java code
-        *       the other is using XML using a file format fxml
+         * A layout means how we arrange all the things in the window
+         * How we configure it ?
+         * Since FX 2.0 we have 2 ways to configure :
+         *       one is using java code
+         *       the other is using XML using a file format fxml
          */
 
         primaryStage.setTitle("Project Information Management");
@@ -76,19 +78,5 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         primaryStage.setMinHeight(150);
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-
-    /*
-    It will be called whenever user interact with the UI
-        + click a myButton --> onClick
-        + ...
-    */
-    @Override
-    public void handle(ActionEvent event) {
-
-        if (event.getSource().equals(myButton)) {
-            logger.info("Button say Hello World is clicked !");
-        }
     }
 }
