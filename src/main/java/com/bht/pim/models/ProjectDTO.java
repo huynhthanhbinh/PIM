6b65a5a85498ff7dfa4e055dbe3dcfec97970055
Richard Huynh <huynhthanhbinh1998@hotmail.com>
2019-07-25 09:00:37 +0700

    TableView Basic

---
+++ src/main/java/com/bht/pim/models/ProjectDTO.java
@@ -0,0 +1,122 @@
+package com.bht.pim.models;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+enum Status {
+    NEW,
+    PLANNED,
+    IN_PROGRESS,
+    FINISHED,
+}
+
+public class ProjectDTO {
+
+    private int id;
+    private long number;
+    private String name;
+    private String customer;
+    private String group;
+    private Status status;
+    private Date start;
+    private Date end;
+    //Map.Entry<String, String> member;
+    private Map<String, String> members;
+
+    public ProjectDTO() {
+        id = 0;
+        number = 0;
+        name = "";
+        customer = "";
+        group = "";
+        status = Status.NEW;
+        start = new Date();
+        end = new Date();
+        members = new HashMap<>();
+    }
+
+    public ProjectDTO(ProjectDTO project) {
+        id = project.id;
+        number = project.number;
+        name = project.name;
+        customer = project.customer;
+        group = project.group;
+        status = project.status;
+        start = project.start;
+        end = project.end;
+        members = project.members;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public Map<String, String> getMembers() {
+        return members;
+    }
+
+    public void setMembers(Map<String, String> members) {
+        this.members = members;
+    }
+}
