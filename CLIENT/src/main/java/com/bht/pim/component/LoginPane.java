f24f04494d7ca94f0c57f1f072b93cd0591db019
BHT <46950058+huynhthanhbinh@users.noreply.github.com>
2019-08-23 11:26:01 +0700

    LoginPane finished

--- CLIENT/src/main/java/com/bht/pim/component/LoginPane.java
+++ CLIENT/src/main/java/com/bht/pim/component/LoginPane.java
@@ -2,14 +2,29 @@ package com.bht.pim.component;
 
 
 import com.bht.pim.configuration.AppConfiguration;
+import com.bht.pim.util.LanguageUtil;
 import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+import org.jacpfx.api.annotations.Resource;
 import org.jacpfx.api.annotations.component.DeclarativeView;
+import org.jacpfx.api.annotations.lifecycle.OnHide;
+import org.jacpfx.api.annotations.lifecycle.OnShow;
+import org.jacpfx.api.annotations.lifecycle.PostConstruct;
+import org.jacpfx.api.annotations.lifecycle.PreDestroy;
 import org.jacpfx.api.message.Message;
 import org.jacpfx.rcp.component.FXComponent;
+import org.jacpfx.rcp.componentLayout.FXComponentLayout;
+import org.jacpfx.rcp.context.Context;
+
+import java.util.ResourceBundle;
 
 @Log4j
 @Getter
@@ -20,6 +35,19 @@ import org.jacpfx.rcp.component.FXComponent;
         viewLocation = "/com/bht/pim/component/LoginPane.fxml")
 public class LoginPane implements FXComponent {
 
+    @Resource
+    private Context context;
+    @FXML
+    private Label lIncorrect;
+    @FXML
+    private Button bLogin;
+    @FXML
+    private Button bForgot;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+
     @Override
     public Node postHandle(Node node, Message<Event, Object> message) throws Exception {
         return null;
@@ -28,5 +56,46 @@ public class LoginPane implements FXComponent {
     @Override
     public Node handle(Message<Event, Object> message) throws Exception {
         return null;
+    }
+
+    @PostConstruct
+    public void onStartComponent(final FXComponentLayout arg0,
+                                 final ResourceBundle resourceBundle) {
+
+        LanguageUtil.initLabel(bLogin.textProperty(), "label.login.login");
+        LanguageUtil.initLabel(bForgot.textProperty(), "label.login.forgot");
+        LanguageUtil.initLabel(lIncorrect.textProperty(), "label.login.incorrect");
+
+        lIncorrect.setVisible(false);
+
+        bLogin.setOnMouseClicked(event -> {
+            if (username.getText().equals(AppConfiguration.USERNAME)
+                    && password.getText().equals(AppConfiguration.PASSWORD)) {
+
+                context.send(AppConfiguration.PERSPECTIVE, "show");
+
+            } else if (!username.getText().isEmpty() && !password.getText().isEmpty()) {
+
+                lIncorrect.setVisible(true);
+            }
+        });
+    }
+
+    @PreDestroy
+    public void onTearDownComponent(final FXComponentLayout componentLayout) {
+        log.info("[DESTROY] FXComponentLayout: " + context.getId());
+    }
+
+    @OnShow
+    public void onShowComponent(final FXComponentLayout componentLayout) {
+        log.info("[SHOW] FXComponentLayout: " + context.getId());
+        username.clear();
+        password.clear();
+        lIncorrect.setVisible(false);
+    }
+
+    @OnHide
+    public void onHide(final FXComponentLayout componentLayout) {
+        log.info("[HIDE] FXComponentLayout: " + context.getId());
     }
 }
