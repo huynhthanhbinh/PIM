8af0fb62c2e58308d907aa99078c6610051d7524
Richard Huynh <huynhthanhbinh1998@hotmail.com>
2019-09-04 09:29:49 +0700

    update fragment to vBox

---
+++ CLIENT/src/main/java/com/bht/pim/util/ViewUtil.java
@@ -0,0 +1,12 @@
+package com.bht.pim.util;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author bht
+ */
+@Log4j
+@Component
+public final class ViewUtil {
+}
