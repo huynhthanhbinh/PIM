295c8003dfe4de74cb5d5a614bc94d39da512da7
Richard Huynh <huynhthanhbinh1998@hotmail.com>
2019-09-24 22:12:04 +0700

    clean code (cont.)

--- CLIENT/src/main/java/com/bht/pim/AppWorkbench.java
+++ CLIENT/src/main/java/com/bht/pim/AppWorkbench.java
@@ -1,23 +1,11 @@
 package com.bht.pim;
 
 import org.jacpfx.api.annotations.workbench.Workbench;
-import org.jacpfx.api.componentLayout.WorkbenchLayout;
-import org.jacpfx.api.message.Message;
-import org.jacpfx.rcp.componentLayout.FXComponentLayout;
-import org.jacpfx.rcp.workbench.FXWorkbench;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.bht.pim.base.BaseBean;
+import com.bht.pim.base.BaseWorkbench;
 import com.bht.pim.perspective.DefaultPerspective;
 import com.bht.pim.perspective.PimPerspective;
 
-import javafx.event.Event;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import lombok.extern.log4j.Log4j;
 
 /**
@@ -25,39 +13,11 @@ import lombok.extern.log4j.Log4j;
  * @author bht
  */
 @Log4j
-@SuppressWarnings("SpringJavaAutowiredMembersInspection")
 @Workbench(id = AppWorkbench.ID, name = AppWorkbench.ID,
         perspectives = {
                 PimPerspective.ID,
                 DefaultPerspective.ID})
-public final class AppWorkbench implements BaseBean, FXWorkbench {
+public final class AppWorkbench extends BaseWorkbench {
 
     public static final String ID = "pimWorkbench";
-    public static final VBox LAYOUT = new VBox();
-
-    @Autowired
-    private AnnotationConfigApplicationContext applicationContext;
-
-    @Override
-    public void handleInitialLayout(
-            Message<Event, Object> message,
-            WorkbenchLayout<Node> layout, Stage stage) {
-
-        //log.info(LoggingUtil.format("INIT", "FXWorkbench", getClass().getSimpleName()));
-        layout.setWorkbenchXYSize(1280, 700);
-        layout.setStyle(StageStyle.DECORATED);
-        layout.setMenuEnabled(false);
-
-        applicationContext.registerShutdownHook(); // for bean destruction of spring @PreDestroy
-    }
-
-
-    @Override
-    public void postHandle(FXComponentLayout componentLayout) {
-        //log.info(LoggingUtil.format("HANDLE", "FXWorkbench", getClass().getSimpleName()));
-
-        LAYOUT.setAlignment(Pos.CENTER);
-        LAYOUT.prefWidthProperty().bind(componentLayout.getGlassPane().widthProperty());
-        LAYOUT.prefHeightProperty().bind(componentLayout.getGlassPane().heightProperty());
-    }
 }
---
+++ CLIENT/src/main/java/com/bht/pim/base/BaseWorkbench.java
@@ -0,0 +1,50 @@
+package com.bht.pim.base;
+
+import org.jacpfx.api.componentLayout.WorkbenchLayout;
+import org.jacpfx.api.message.Message;
+import org.jacpfx.rcp.componentLayout.FXComponentLayout;
+import org.jacpfx.rcp.workbench.FXWorkbench;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javafx.event.Event;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author bht
+ */
+@SuppressWarnings("SpringJavaAutowiredMembersInspection")
+public abstract class BaseWorkbench implements FXWorkbench, BaseBean {
+
+    public static final VBox LAYOUT = new VBox();
+
+    @Autowired
+    private AnnotationConfigApplicationContext applicationContext;
+
+    @Override
+    public void handleInitialLayout(
+            Message<Event, Object> message,
+            WorkbenchLayout<Node> layout, Stage stage) {
+
+        //log.info(LoggingUtil.format("INIT", "FXWorkbench", getClass().getSimpleName()));
+        layout.setWorkbenchXYSize(1280, 700);
+        layout.setStyle(StageStyle.DECORATED);
+        layout.setMenuEnabled(false);
+
+        applicationContext.registerShutdownHook(); // for bean destruction of spring @PreDestroy
+    }
+
+    @Override
+    public void postHandle(FXComponentLayout componentLayout) {
+
+        //log.info(LoggingUtil.format("HANDLE", "FXWorkbench", getClass().getSimpleName()));
+        LAYOUT.setAlignment(Pos.CENTER);
+        LAYOUT.prefWidthProperty().bind(componentLayout.getGlassPane().widthProperty());
+        LAYOUT.prefHeightProperty().bind(componentLayout.getGlassPane().heightProperty());
+    }
+}
