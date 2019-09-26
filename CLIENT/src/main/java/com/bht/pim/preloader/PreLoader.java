79dd74336fcfa184d9944b69265fcd4167b0e98a
Richard Huynh <huynhthanhbinh1998@hotmail.com>
2019-09-26 17:17:54 +0700

    [preloader] clean code

--- CLIENT/src/main/java/com/bht/pim/preloader/PreLoader.java
+++ CLIENT/src/main/java/com/bht/pim/preloader/PreLoader.java
@@ -4,45 +4,60 @@ import java.io.IOException;
 import java.util.Objects;
 
 import com.bht.pim.util.ImageUtil;
+import com.bht.pim.util.LoggingUtil;
 
 import javafx.application.Preloader;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import lombok.extern.log4j.Log4j;
 
 /**
  *
  * @author bht
  */
+@Log4j
 public class PreLoader extends Preloader {
 
     private static final String PRELOADER_PATH = "com/bht/pim/preloader/PreLoader.fxml";
-    private static Stage loaderStage;
+    private static final int PANE_WIDTH = 500;
+    private static final int PANE_HEIGHT = 300;
+    private static Stage preLoaderStage;
 
     @Override
     public synchronized void start(Stage primaryStage) throws IOException {
-        AnchorPane anchorPane = FXMLLoader.load(Objects
-                .requireNonNull(getClass().getClassLoader().getResource(PRELOADER_PATH)));
-        Scene scene = new Scene(anchorPane, 500, 300);
-
-        loaderStage = primaryStage;
-        primaryStage.setScene(scene);
-        primaryStage.setWidth(500);
-        primaryStage.setHeight(300);
-        primaryStage.setResizable(false);
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setAlwaysOnTop(true);
-        primaryStage.getIcons().add(ImageUtil.getImage("icon"));
-        primaryStage.show();
-        primaryStage.centerOnScreen();
+        log.info(LoggingUtil.format("INFO", "PreLoader", "show preloader pane"));
+        preLoaderStage = primaryStage;
+        initPreLoaderStage();
     }
 
+    private void initPreLoaderStage() throws IOException {
+        preLoaderStage.setScene(getPreloaderScene());
+        preLoaderStage.setWidth(PANE_WIDTH);
+        preLoaderStage.setHeight(PANE_HEIGHT);
+        preLoaderStage.setResizable(false);
+        preLoaderStage.initStyle(StageStyle.UNDECORATED);
+        preLoaderStage.setAlwaysOnTop(true);
+        preLoaderStage.getIcons().add(ImageUtil.getImage("icon"));
+        preLoaderStage.show();
+        preLoaderStage.centerOnScreen();
+    }
+
+    private Scene getPreloaderScene() throws IOException {
+        return new Scene(
+                FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource(PRELOADER_PATH))),
+                PANE_WIDTH,
+                PANE_HEIGHT);
+    }
+
+    // preloader pane will be hide on workbench finished initialing
     public static void closePreloader() {
-        if (loaderStage != null) {
-            loaderStage.close();
-            loaderStage = null;
+        log.info(LoggingUtil.format("INFO", "PreLoader", "hide preloader pane"));
+
+        if (preLoaderStage != null) {
+            preLoaderStage.close();
+            preLoaderStage = null;
         }
     }
 }
