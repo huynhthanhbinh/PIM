51e5f059520f1217402d5b16cddca7055500cc34
Richard Huynh <huynhthanhbinh1998@hotmail.com>
2019-10-11 21:35:42 +0700

    temporary commit of util update (cont.)

--- CLIENT/src/main/java/com/bht/pim/property/FormatProperty.java
+++ CLIENT/src/main/java/com/bht/pim/property/FormatProperty.java
@@ -1,15 +1,19 @@
 package com.bht.pim.property;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.bht.pim.base.BaseBean;
+import com.bht.pim.util.FormatUtil;
 import com.bht.pim.util.LanguageUtil;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.util.StringConverter;
 
 /**
  *
@@ -33,6 +37,12 @@ public final class FormatProperty implements BaseBean {
     private void addEventListener() {
         languageProperty.getLocaleProperty()
                 .addListener((observable, oldLocale, newLocale) -> reloadDatePattern());
+
+        FormatUtil.DATE_FORMATTER_PROPERTY.addListener(observable -> FormatUtil.DATE_STRING_CONVERTER.set(dateStringConverter()));
+        FormatUtil.DATE_FORMATTER_PROPERTY.set(DateTimeFormatter.ofPattern(FormatProperty.DATE_PATTERN_PROPERTY.get()));
+
+        FormatProperty.DATE_PATTERN_PROPERTY.addListener((observable, oldValue, newValue) ->
+                FormatUtil.DATE_FORMATTER_PROPERTY.set(DateTimeFormatter.ofPattern(newValue)));
     }
 
     private void initDatePatternProperty() {
@@ -41,5 +51,36 @@ public final class FormatProperty implements BaseBean {
 
     private void reloadDatePattern() {
         DATE_PATTERN_PROPERTY.set(LanguageUtil.getCurrentLabelOfKey("pattern.date"));
+    }
+
+    // Converter for pim date-picker controls
+    private static StringConverter<LocalDate> dateStringConverter() {
+        return new StringConverter<LocalDate>() {
+
+            @Override
+            public String toString(LocalDate localDate) {
+                return getString(localDate);
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                if (string == null || string.trim().isEmpty()) {
+                    return null;
+                }
+                try {
+                    return LocalDate.parse(string, FormatUtil.DATE_FORMATTER_PROPERTY.get());
+
+                } catch (Exception exception) {
+
+                    return null;
+                }
+            }
+        };
+    }
+
+    public static String getString(LocalDate localDate) {
+        return (localDate != null)
+                ? FormatUtil.DATE_FORMATTER_PROPERTY.get().format(localDate)
+                : "";
     }
 }
--- CLIENT/src/main/java/com/bht/pim/util/FormatUtil.java
+++ CLIENT/src/main/java/com/bht/pim/util/FormatUtil.java
@@ -3,10 +3,6 @@ package com.bht.pim.util;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.stereotype.Component;
-
 import com.bht.pim.property.FormatProperty;
 
 import javafx.beans.property.ObjectProperty;
@@ -19,83 +15,19 @@ import javafx.util.StringConverter;
  *
  * @author bht
  */
-@Component
 public final class FormatUtil {
 
-    public static final ObjectProperty<StringConverter<LocalDate>> DATE_STRING_CONVERTER = new SimpleObjectProperty<>();
-    private static final ObjectProperty<DateTimeFormatter> DATE_FORMATTER_PROPERTY = new SimpleObjectProperty<>();
-
-    @PostConstruct
-    public void addAllEventListeners() {
-        DATE_FORMATTER_PROPERTY.addListener(observable -> DATE_STRING_CONVERTER.set(dateStringConverter()));
-        DATE_FORMATTER_PROPERTY.set(DateTimeFormatter.ofPattern(FormatProperty.DATE_PATTERN_PROPERTY.get()));
-
-        FormatProperty.DATE_PATTERN_PROPERTY.addListener((observable, oldValue, newValue) ->
-                DATE_FORMATTER_PROPERTY.set(DateTimeFormatter.ofPattern(newValue)));
+    private FormatUtil() {
     }
 
-//    public static <T> ObjectProperty<Callback<TableColumn<T, LocalDate>, TableCell<T, LocalDate>>> getDateCellFormatProperty() {
-//        ObjectProperty<Callback<TableColumn<T, LocalDate>, TableCell<T, LocalDate>>> dateCellFormatProperty = new SimpleObjectProperty<>();
-//        dateCellFormatProperty.set(FormatUtil::dateCellFormat);
-//        DATE_FORMATTER_PROPERTY.addListener(observable -> {
-//            dateCellFormatProperty.set(FormatUtil::dateCellFormat);
-//        });
-//        return dateCellFormatProperty;
-//    }
-//
-//
-//    // Format Date : convert from Timestamp to LocalDate
-//    private static <T> TableCell<T, LocalDate> dateCellFormat(TableColumn<T, LocalDate> column) {
-//        return new TableCell<T, LocalDate>() {
-//
-//            boolean isBound = false;
-//
-//            @Override
-//            protected void updateItem(LocalDate localDate, boolean empty) {
-//                if (!isBound) {
-//                    textProperty().bind(toStringProperty(localDate));
-//                }
-//            }
-//        };
-//    }
-
+    public static final ObjectProperty<StringConverter<LocalDate>> DATE_STRING_CONVERTER = new SimpleObjectProperty<>();
+    public static final ObjectProperty<DateTimeFormatter> DATE_FORMATTER_PROPERTY = new SimpleObjectProperty<>();
 
     public static StringProperty toStringProperty(LocalDate localDate) {
         StringProperty stringProperty = new SimpleStringProperty();
-        stringProperty.set(getString(localDate));
-        DATE_FORMATTER_PROPERTY.addListener(observable -> stringProperty.set(getString(localDate)));
+        stringProperty.set(FormatProperty.getString(localDate));
+        DATE_FORMATTER_PROPERTY.addListener(observable ->
+                stringProperty.set(FormatProperty.getString(localDate)));
         return stringProperty;
-    }
-
-    private static String getString(LocalDate localDate) {
-        return (localDate != null)
-                ? DATE_FORMATTER_PROPERTY.get().format(localDate)
-                : "";
-    }
-
-
-    // Converter for pim date-picker controls
-    private StringConverter<LocalDate> dateStringConverter() {
-        return new StringConverter<LocalDate>() {
-
-            @Override
-            public String toString(LocalDate localDate) {
-                return getString(localDate);
-            }
-
-            @Override
-            public LocalDate fromString(String string) {
-                if (string == null || string.trim().isEmpty()) {
-                    return null;
-                }
-                try {
-                    return LocalDate.parse(string, DATE_FORMATTER_PROPERTY.get());
-
-                } catch (Exception exception) {
-
-                    return null;
-                }
-            }
-        };
     }
 }
